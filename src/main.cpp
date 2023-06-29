79ea71a80192a16c725acc39547079ee378cd63e
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-06-28 23:41:07 -0300

    program: Blink with Button

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,39 @@
 #include <Arduino.h>
+//Press the Button to turn both LEDs ON/OFF
 
-// put function declarations here:
+// defines
+#define LED1 7 //LED Digital Port 7
+#define LED2 8 // LED Digital Port 8
+#define BUTTON1 9 // Button Digital Port 9
+
+//global var
+unsigned int state = 0; //change LED state
+unsigned int pressB = 0; //button presses
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  //Pins Setup
+  pinMode(LED1,OUTPUT);
+  digitalWrite(LED1, LOW);
+  pinMode(LED2,OUTPUT);
+  digitalWrite(LED2, LOW);
+  pinMode(BUTTON1, INPUT);
+  Serial.begin(9600);
+  Serial.println("Begin...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //check if the button is pressed then change the state of the LEDs to ON/OFF
+  if(digitalRead(BUTTON1))
+  {
+    state = !state;
+    Serial.println("Button");
+    digitalWrite(LED1,state);
+    digitalWrite(LED2,state);
+    pressB++; //increment button press
+    Serial.println(pressB);
+    delay(100); //debounce
+  }
+  delay(100); //debounce
 }
 
-// put function definitions here:
