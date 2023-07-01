b676dd70f8665643f7c04fba9b762fabb6008d60
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-07-01 15:26:23 -0300

    debounce-button

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,89 @@
 #include <Arduino.h>
-//Press the Button to turn both LEDs ON/OFF
+/*
+  Debounce
 
+  Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
+  press), the output pin is toggled from LOW to HIGH or HIGH to LOW. There's a
+  minimum delay between toggles to debounce the circuit (i.e. to ignore noise).
+
+  The circuit:
+  - LED attached from pin 13 to ground through 220 ohm resistor
+  - pushbutton attached from pin 2 to +5V
+  - 10 kilohm resistor attached from pin 2 to ground
+
+  - Note: On most Arduino boards, there is already an LED on the board connected
+    to pin 13, so you don't need any extra components for this example.
+
+  created 21 Nov 2006
+  by David A. Mellis
+  modified 30 Aug 2011
+  by Limor Fried
+  modified 28 Dec 2012
+  by Mike Walters
+  modified 30 Aug 2016
+  by Arturo Guadalupi
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Debounce
+*/
 // defines
 #define LED1 7 //LED Digital Port 7
 #define LED2 8 // LED Digital Port 8
 #define BUTTON1 9 // Button Digital Port 9
 
-//global var
-unsigned int state = 0; //change LED state
-unsigned int pressB = 0; //button presses
+// Variables will change:
+int ledState = HIGH;        // the current state of the output pin
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW;  // the previous reading from the input pin
 
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 void setup() {
-  //Pins Setup
   pinMode(LED1,OUTPUT);
   digitalWrite(LED1, LOW);
   pinMode(LED2,OUTPUT);
   digitalWrite(LED2, LOW);
   pinMode(BUTTON1, INPUT);
-  Serial.begin(9600);
-  Serial.println("Begin...");
+
 }
 
 void loop() {
-  //check if the button is pressed then change the state of the LEDs to ON/OFF
-  if(digitalRead(BUTTON1))
-  {
-    state = !state;
-    Serial.println("Button");
-    digitalWrite(LED1,state);
-    digitalWrite(LED2,state);
-    pressB++; //increment button press
-    Serial.println(pressB);
-    delay(100); //debounce
-  }
-  delay(100); //debounce
-}
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(BUTTON1);
 
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  // set the LED:
+  digitalWrite(LED1, ledState);
+  digitalWrite(LED2, ledState);
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+}
